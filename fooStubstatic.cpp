3fa8ce0d3277d7aa7b7a273f345308a6b2ef0479
Nguyen Trong Dat <datntrong@gmail.com>
2022-03-30 10:19:31 +0700

    fix bug

--- fooStubstatic.cpp
+++ fooStubstatic.cpp
@@ -5,17 +5,17 @@ void sayHello1(){
     cout << "Hello" << endl;
 }
 
-static int return0(){
+static int return0_static(){
     return 0;
 }
 
-static int return1(){
+static int return1_static(){
     return 1;
 }
 
 int foo1_static(int a){
     int b[2] = {a, a + 1};
-    int tmp = return0();
+    int tmp = return0_static();
     if(tmp == -1){
         return a;
     } else {
@@ -25,13 +25,13 @@ int foo1_static(int a){
 
 int foo2_static(int a){
     sayHello1();
-    int tmp = return0(), tmp2 = return0()*2, tmp3 = return0()-6+return1();
-    tmp = return1();
-    tmp = return0() + return1();
+    int tmp = return0_static(), tmp2 = return0_static()*2, tmp3 = return0_static()-6+return1_static();
+    tmp = return1_static();
+    tmp = return0_static() + return1_static();
     if(tmp == -1){
         return a;
     } else {
-        if(return0() == -2){
+        if(return0_static() == -2){
             return a + 1;
         } else {
             return 0;
