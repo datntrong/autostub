e9c258232aee5e0a7652f17b047087bf4fb36709
Nguyen Trong Dat <datntrong@gmail.com>
2022-04-04 08:55:46 +0700

    Update foostatic parameter

--- fooStubstatic.cpp
+++ fooStubstatic.cpp
@@ -13,6 +13,12 @@ static int return1_static(){
     return 1;
 }
 
+static int return1_static_para(int a){
+    return 1;
+}
+static int return0_static_para(int a){
+    return 0;
+}
 int foo1_static(int a){
     int b[2] = {a, a + 1};
     int tmp = return0_static();
@@ -34,6 +40,24 @@ int foo2_static(int a){
     } else {
         cout<< "if false";
         if(return0_static() == -2){
+            return a + 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
+int foo2_static_para(int a){
+    sayHello1();
+    int tmp = return0_static_para(a), tmp2 = return0_static_para(a)*2, tmp3 = return0_static_para(a)-6+return1_static_para(a);
+    tmp = return1_static_para(a);
+    tmp = return0_static_para(a) + return1_static_para(a);
+    if(tmp == -1){
+        cout<< "if true";
+        return a;
+    } else {
+        cout<< "if false";
+        if(return0_static_para(a) == -2){
             return a + 1;
         } else {
             return 0;
