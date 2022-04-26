62557ce0aee18ec7995064c9d4c472c50a1366dc
HoangTrang150302 <hoangtrang150302@gmail.com>
2022-04-26 13:32:42 +0700

    update class A_overload

--- classoverloadfunction.cpp
+++ classoverloadfunction.cpp
@@ -14,6 +14,18 @@ public:
         cout<<"I am A_foo-double"<<endl;
         return 0;
     }
+    int foo(long a){
+        cout<<"I am A_foo_long"<<endl;
+        return 0;
+    }
+    int foo_1(string a){
+        cout<<"I am A_foo_1_string"<<endl;
+        return 0;    
+    }
+    int foo_1(char a){
+        cout<<"I am A_foo_1_char"<<endl;
+        return 0;    
+    }
 };
 
 class B_overload {
@@ -37,8 +49,6 @@ class B_overload {
             return this->objA;
         }
 };
-
-
 
 int uutFuncoverload(int a, double b) {
     A_overload objA;
@@ -82,5 +92,29 @@ int multipleReferenceoverload(int a, double b) {
         cout << -2 << endl;
     }
     return 0;
-
 }
+
+// int foo_stub_int(void* obj,int a)
+// {   
+//     A* o= (A*)obj;
+//     cout<<"I am foo_stub_int"<< a << endl;
+//     return 0;
+// }
+
+// int foo_stub_double(void* obj,double a)
+// {   
+//     A* o= (A*)obj;
+//     cout<<"I am foo_stub_double"<< a << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     Stub stub;
+//     stub.set((int(A::*)(int))ADDR(A,foo), foo_stub_int);
+//     stub.set((int(A::*)(double))ADDR(A,foo), foo_stub_double);
+//     A a;
+//     a.foo(5);
+//     a.foo(1.1);
+//     return 0;
+// }
