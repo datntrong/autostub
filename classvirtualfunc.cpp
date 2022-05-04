e35c93e647564d5377e9593fd4a6a3203cc13169
Hieubentau <hieuvuhv34@gmail.com>
2022-05-04 16:45:12 +0700

    Add testcase classvirtual

--- classvirtualfunc.cpp
+++ classvirtualfunc.cpp
@@ -1,45 +1,85 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class A_virtual{
+class A_virtual
+{
     int i;
+
 public:
-    virtual int ret0_virtual(int a){
-        cout<<"I am A_ret0_virtual"<<endl;
+    virtual int ret0_virtual(int a)
+    {
+        cout << "I am A_ret0_virtual" << endl;
         return 0;
     }
-    virtual int ret1_virtual(){
-        cout<<"I am A_ret1_virtual"<<endl;
+    virtual int ret1_virtual()
+    {
+        cout << "I am A_ret1_virtual" << endl;
         return 1;
     }
-    virtual int ret5_virtual(){
-        cout<<"I am A_ret1_virtual"<<endl;
+    virtual int ret5_virtual()
+    {
+        cout << "I am A_ret1_virtual" << endl;
         return 5;
     }
 };
 
-class B_virtual:A_virtual{
-    public:
-    int ret0_virtual(int a){
-        cout<<"I am A_ret0_virtual"<<endl;
+class B_virtual : A_virtual
+{
+public:
+    int ret0_virtual(int a)
+    {
+        cout << "I am A_ret0_virtual" << endl;
         return 0;
     }
-    int ret1_virtual(){
-        cout<<"I am A_ret1_virtual"<<endl;
+    int ret1_virtual()
+    {
+        cout << "I am A_ret1_virtual" << endl;
         return 1;
+    }
+    int ret2_virtual()
+    {
+        cout << "I am A_ret2_virtual" << endl;
+        return 2;
     }
 };
 
-int uutFuncvirtual(int a, double b) {
+class C_virtual : A_virtual, B_virtual
+{
+public:
+    int ret0_virtual(int *a)
+    {
+        cout << "I am A_ret0_virtual" << endl;
+        return 0;
+    }
+    int ret1_virtual()
+    {
+        cout << "I am A_ret1_virtual" << endl;
+        return 1;
+    }
+    int ret2_virtual(int a)
+    {
+        cout << "I am A_ret2_virtual" << endl;
+        return 2;
+    }
+}
+
+int uutFuncvirtual(int a, double b)
+{
     A_virtual objA;
-    if (objA.ret0_virtual(a)) {
+    if (objA.ret0_virtual(a))
+    {
         cout << 1 << endl;
-    } else {
+    }
+    else
+    {
         cout << -1 << endl;
     }
 
-    if (objA.ret1_virtual()) {
+    if (objA.ret1_virtual())
+    {
         cout << 2 << endl;
-    } else {
+    }
+    else
+    {
         cout << -2 << endl;
     }
     return 0;
@@ -49,18 +89,25 @@ int uutFuncvirtual(int a, double b) {
     //   A::fun2(); // Allowed
 }
 
-int uutFuncvirtual_nonpara(int a, double b) {
+int uutFuncvirtual_nonpara(int a, double b)
+{
     A_virtual objA;
 
-    if (objA.ret1_virtual()) {
+    if (objA.ret1_virtual())
+    {
         cout << 2 << endl;
-    } else {
+    }
+    else
+    {
         cout << -2 << endl;
     }
 
-    if (objA.ret5_virtual()==6) {
+    if (objA.ret5_virtual() == 6)
+    {
         cout << 2 << endl;
-    } else {
+    }
+    else
+    {
         cout << -2 << endl;
     }
     return 0;
@@ -70,26 +117,67 @@ int uutFuncvirtual_nonpara(int a, double b) {
     //   A::fun2(); // Allowed
 }
 
-int multipleReferencevirtual(int a, double b) {
+int multipleReferencevirtual(int a, double b)
+{
     A_virtual objA;
     B_virtual obj;
-    if (obj.ret0_virtual(a) == 2) {
+    if (obj.ret0_virtual(a) == 2)
+    {
         cout << 1 << endl;
-    } else {
+    }
+    else
+    {
         cout << -1 << endl;
     }
 
-    if (obj.ret0_virtual(a) == 2) {
+    if (obj.ret0_virtual(a) == 2)
+    {
         cout << 2 << endl;
-    } else {
+    }
+    else
+    {
         cout << -2 << endl;
     }
 
-    if (objA.ret1_virtual() == 6) {
+    if (objA.ret1_virtual() == 6)
+    {
         cout << 2 << endl;
-    } else {
+    }
+    else
+    {
         cout << -2 << endl;
     }
     return 0;
+}
 
+int multipleInheritance(int *a, int b)
+{
+    C_virtual objC;
+    if (objC.ret0_virtual(a) == 0)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+
+    if (objC.ret1_virtual() == 1)
+    {
+        cout << 2 << endl;
+    }
+    else
+    {
+        cout << -2 << endl;
+    }
+
+    if (objC.ret2_virtual() == 2)
+    {
+        cout << 3 << endl;
+    }
+    else
+    {
+        cout << -3 << endl;
+    }
+    return 0;
 }
