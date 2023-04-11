ef677a4fdb864aeb91b7d6eb8dec0ff56b495180
hiepuet1705 <100411662+hiepuet1705@users.noreply.github.com>
2023-04-11 15:50:36 +0700

    update 2 para

--- fooStub.cpp
+++ fooStub.cpp
@@ -31,6 +31,11 @@ int return1_para(int ){
     return 1;
 }
 
+int return0_2_para(int a,int b){
+    
+    return 0;
+}
+
 int foo2(int a){
     sayHello();
     int tmp = return0(), tmp2 = return0()*2, tmp3 = return0()-6+return1();
@@ -66,6 +71,23 @@ int foo2_para(int a){
 
 }
 
+
+int foo2_para(int a,int b){
+    sayHello();
+    int tmp = return0_2_para(a,b), tmp2 = return0_para(a)*2, tmp3 = return0_para(a)-6+return1();
+    tmp = return1_para(a);
+    tmp = return0_para(a) + return1();
+    if(tmp == -1){
+        return a;
+    } else {
+        if(return0_para(a) == -2){
+            return a + 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
 int foo3(int a){
     int tmp = a;
     if(tmp == 1){
